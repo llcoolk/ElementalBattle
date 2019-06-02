5d596cb313297772af5ad6742c380671857510fb
Leen Kim <leenkim@alumni.usc.edu>
2019-06-02 01:16:49 -0700

    add components button, game, and login

--- ElementalBattles/frontend/src/components/App/App.jsx
+++ ElementalBattles/frontend/src/components/App/App.jsx
@@ -1,11 +1,14 @@
-// React core
+
 import React, { Component } from 'react';
+//Components
+import { Login } from 'components';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+        <Login />
       </div>
     );
   }
--- ElementalBattles/frontend/src/components/App/index.js
+++ ElementalBattles/frontend/src/components/App/index.js
@@ -1,3 +1,4 @@
+import './App.css';
 import App from './App';
 
 export default App;
